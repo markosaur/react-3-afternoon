a29beaa80b71b67e5f49d9d2bc001d4a9745607b
Markosaur <mark3pace@gmail.com>
2019-09-04 23:29:41 -0600

    This is complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
@@ -19,18 +21,29 @@ class App extends Component {
   }
   
   componentDidMount() {
+  Axios.get('https://practiceapi.devmountain.com/api/posts').then( response => {
+    this.setState({posts: response.data});
+  });
 
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    Axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text }).then( results => {
+      this.setState({posts: results.data});
+    });
   
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    Axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(results=>{
+      this.setState({posts: results.data});
+    });
   }
 
-  createPost() {
+  createPost(text) {
+    Axios.post(`https://practiceapi.devmountain.com/api/posts`, {text}).then(results=>{
+      this.setState({posts: results.data})
+    })
 
   }
 
@@ -43,7 +56,19 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn = {this.createPost} />
+
+          {
+            posts.map( (post) => (
+              <Post key={ post.id } 
+                    text={post.text}
+                    date={post.date}
+                    id ={ post.id }
+                    updatePostFn={ this.updatePost }
+                    deletePostFn={ this.deletePost } />
+                    
+            ))
+          }
           
         </section>
       </div>
